ff04c5db3871fffbb0c5bbb13b6d638b987fde58
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-12-07 22:53:39 +0000

    Busca sequencial

--- .ccls-cache/@home@runner@EstruturasDeDadosEmC-BuscasSequenciais/main.c
+++ .ccls-cache/@home@runner@EstruturasDeDadosEmC-BuscasSequenciais/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int v[10] = {4,8,2,56,7,98,1,25,33,78};
+
 int main(void) {
-  printf("Hello World\n");
+
+	//Busca sequencial
+	int num = 98;
+	for(int i=0; i<10; i++){
+		if(v[i]==num){
+			printf("Elemento encontrado no posição %d", i);
+		}
+	}
+
+	
   return 0;
 }
